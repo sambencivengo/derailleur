a62ecb177dc60efe6bad37bcd0e99cf1333b422b
Sam Bencivengo <sambencivengo@gmail.com>
2023-09-14 22:37:44 -0600

    routes connecting

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
 'use client';
 
 export default function Page() {
-  // const createUser = async (e: React.SyntheticEvent) => {
-  //   e.preventDefault();
-  //   try {
-  //     const body = { username: 'sammy' };
-  //     await fetch(`/api/user`, {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify(body),
-  //     });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
   const createUser = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     try {
-      await fetch(`/api/hello`, {});
+      const body = { username: 'sammy' };
+      await fetch(`/api/user`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
     } catch (error) {
       console.error(error);
     }
   };
+  // const createUser = async (e: React.SyntheticEvent) => {
+  //   e.preventDefault();
+  //   try {
+  //     await fetch(`/api/hello`, {});
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   return (
     <>
