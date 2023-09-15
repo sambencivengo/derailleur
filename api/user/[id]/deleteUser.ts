9cb4151c1694b63bc0fe8c031b59ebbedde4d24a
Sam Bencivengo <sambencivengo@gmail.com>
2023-09-14 20:57:39 -0600

    user rest methods

---
+++ api/user/[id]/deleteUser.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../prisma";
+
+
+
+export default async function deleteUser(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+
+  const user = await prisma.user.delete({
+    where: {
+      id: Number(id)
+    }
+  });
+  // TODO: catch error or if there is no user found
+
+  console.log('👤 User Deleted', user);
+  return res.status(200).json("User Deleted");
+}
---
+++ api/user/[id]/findUser.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../prisma";
+
+export default async function findUser(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(id)
+    }
+  }); // TODO: catch Errors
+
+  console.log('👤 User Found', user);
+
+  return res.status(201).json(user);
+}
