bcf0261c3af4682b78235c17f376269601121873
Sam Bencivengo <sambencivengo@gmail.com>
2023-09-19 12:54:49 -0600

    max character length and test for username length

--- app/__test__/mock/mockUser.ts
+++ app/__test__/mock/mockUser.ts
@@ -1,7 +1,8 @@
 import { User } from "../../../types/user";
 
-
 export const mockUser_00: User = {
   id: 'testUserId_00',
-  username: 'sammy'
+  username: 'sammy',
+  createdAt: new Date('2023-09-19'),
+  updatedAt: new Date('2023-09-19')
 };
--- app/__test__/user/createUser.test.ts
+++ app/__test__/user/createUser.test.ts
@@ -5,25 +5,34 @@ import { cleanUpTable } from "../utils/cleanUpDatabase";
 import prisma from "../../../prisma/prisma";
 
 describe('Create User Query', function () {
-
-  const testUsername1 = 'username';
-  const testUserId1 = uuid();
+  const testUsername = 'username';
   const now = new Date();
 
   it("Successfully creates a new user", async function () {
-    const response = await createUser({ username: testUsername1 }, testUserId1);
+    const testUserId = uuid();
+    const response = await createUser({ username: testUsername }, testUserId);
     const result = response.result!;
-    assert.strictEqual(result.username, testUsername1);
-    assert.strictEqual(result.id, testUserId1);
+    assert.strictEqual(result.username, testUsername);
+    assert.strictEqual(result.id, testUserId);
     assert(now < result.createdAt);
     assert(now < result.updatedAt);
   });
   it("Fails to create a user due to username conflict", async function () {
-    const response = await createUser({ username: testUsername1 });
+    const response = await createUser({ username: testUsername });
+    const { error, result } = response;
+    assert.strictEqual(result, null);
+    assert.strictEqual(typeof error, 'object');
+    // TODO: type out all Prisma errors
+  });
+  it("Fails to create a user due to username being greater than 30 characters", async function () {
+    const username = 'x'.repeat(31);
+    const response = await createUser({ username });
     const { error, result } = response;
     assert.strictEqual(result, null);
     assert.strictEqual(typeof error, 'object');
   });
+
+
 
   afterAll(async function () {
     await cleanUpTable(prisma.users);
