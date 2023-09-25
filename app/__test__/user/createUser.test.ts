102cb3bc3eabe59d2fa932a78f3f9012192bf9ba
Sam Bencivengo <sambencivengo@gmail.com>
2023-09-25 16:31:54 -0600

    some tests have been re-added

--- app/__test__/user/createUser.test.ts
+++ app/__test__/user/createUser.test.ts
@@ -1,15 +1,14 @@
 import assert from "assert";
-// import prisma from "../../../prisma/prisma";
-import { faker } from '@faker-js/faker';
+import prisma from "../../../prisma/prisma";
 import { CreateUserPayload, createUser } from "../../queries/user/createUser";
 import { v4 as uuid } from 'uuid';
-// import { cleanUpTable } from "../utils/cleanUpDatabase";
-// import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-// import { PrismaQueryErrorCodes } from "../../../prisma/prismaErrorCodes";
+import { cleanUpTable } from "../utils/cleanUpDatabase";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { PrismaQueryErrorCodes } from "../../../prisma/prismaErrorCodes";
 
 
 describe('Create User Query', function () {
-  const testUsername = faker.person.firstName();
+  const testUsername = 'sammy_single_track';
   const now = new Date();
   const testUserId = uuid();
 
@@ -22,7 +21,6 @@ describe('Create User Query', function () {
       location: testLocation
     };
     const response = await createUser(testCreateUser, testUserId);
-    console.log(response);
     const result = response.result!;
     assert.strictEqual(result.username, testUsername);
     assert.strictEqual(result.id, testUserId);
@@ -31,44 +29,43 @@ describe('Create User Query', function () {
     assert(now < result.createdAt);
     assert(now < result.updatedAt);
   });
-  // it("Successfully creates a new user with location and favorite bike undefined", async function () {
-  //   const username = 'testUsername_01';
-  //   const response = await createUser({ username });
-  //   const result = response.result!;
-  //   assert.strictEqual(result.username, username);
-  //   assert.strictEqual(result.favoriteBike, null);
-  //   assert.strictEqual(result.location, null);
-  //   assert(now < result.createdAt);
-  //   assert(now < result.updatedAt);
-  // });
-  // it("Fails to create a user due to username conflict", async function () {
-  //   const response = await createUser({ username: testUsername });
-  //   const { error, result } = response;
-  //   assert.strictEqual(result, null);
-  //   assert.strictEqual(typeof error, 'object');
-  // });
-  // it("Fails to create a user due to username being greater than 30 characters", async function () {
-  //   const username = 'x'.repeat(31);
-  //   const response = await createUser({ username });
-  //   const { result } = response;
-  //   const error: PrismaClientKnownRequestError = response.error;
-  //   assert.strictEqual(result, null);
-  //   assert.strictEqual(typeof error, 'object');
-  //   assert.strictEqual(error.code, PrismaQueryErrorCodes.VALUE_TOO_LONG);
-  // });
-  // it("Fails to create a user due to being supplied a non-unique uuid()", async function () {
-  //   const username = 'testUsername_02';
-  //   const response = await createUser({ username }, testUserId);
-  //   const { result } = response;
-  //   const error: PrismaClientKnownRequestError = response.error;
-  //   assert.strictEqual(result, null);
-  //   assert.strictEqual(typeof error, 'object');
-  //   assert.strictEqual(error.code, PrismaQueryErrorCodes.UNIQUE_CONSTRAINT);
-  // });
+  it("Successfully creates a new user with location and favorite bike undefined", async function () {
+    const username = 'testUsername_01';
+    const response = await createUser({ username });
+    const result = response.result!;
+    assert.strictEqual(result.username, username);
+    assert.strictEqual(result.favoriteBike, null);
+    assert.strictEqual(result.location, null);
+    assert(now < result.createdAt);
+    assert(now < result.updatedAt);
+  });
+  it("Fails to create a user due to username conflict", async function () {
+    const response = await createUser({ username: testUsername });
+    const { error, result } = response;
+    assert.strictEqual(result, null);
+    assert.strictEqual(typeof error, 'object');
+  });
+  it("Fails to create a user due to username being greater than 30 characters", async function () {
+    const username = 'x'.repeat(31);
+    const response = await createUser({ username });
+    const { result } = response;
+    const error: PrismaClientKnownRequestError = response.error;
+    assert.strictEqual(result, null);
+    assert.strictEqual(typeof error, 'object');
+    assert.strictEqual(error.code, PrismaQueryErrorCodes.VALUE_TOO_LONG);
+  });
+  it("Fails to create a user due to being supplied a non-unique uuid()", async function () {
+    const username = 'testUsername_02';
+    const response = await createUser({ username }, testUserId);
+    const { result } = response;
+    const error: PrismaClientKnownRequestError = response.error;
+    assert.strictEqual(result, null);
+    assert.strictEqual(typeof error, 'object');
+    assert.strictEqual(error.code, PrismaQueryErrorCodes.UNIQUE_CONSTRAINT);
+  });
 
   afterAll(async function () {
-
-    // await cleanUpTable(prisma.users);
+    await cleanUpTable([prisma.users]);
   });
 });
 
--- app/__test__/utils/cleanUpDatabase.ts
+++ app/__test__/utils/cleanUpDatabase.ts
@@ -1,8 +1,11 @@
 import { Prisma } from "@prisma/client";
 import { DefaultArgs } from "@prisma/client/runtime/library";
+import prisma from "../../../prisma/prisma";
 
-export type PrismaTable = Prisma.UsersDelegate<DefaultArgs> | Prisma.UsersDelegate<DefaultArgs>;
+export type PrismaUsers = typeof prisma.users;
+export type PrismaPosts = typeof prisma.posts;
+export type PrismaTable = PrismaUsers | Prisma.UsersDelegate<DefaultArgs>;
 
-export async function cleanUpTable(prismaTable: PrismaTable) {
-  await prismaTable.deleteMany({});
+export async function cleanUpTable(table: PrismaTable[]) {
+  await Promise.all(table.map((table) => table.deleteMany({})));
 }
