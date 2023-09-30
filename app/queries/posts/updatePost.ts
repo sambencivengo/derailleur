d940e82ef283fda39302277ca46c56d61c55429d
Sam Bencivengo <sambencivengo@gmail.com>
2023-09-29 23:14:48 -0600

    update post funciton

---
+++ app/queries/posts/updatePost.ts
@@ -0,0 +1,34 @@
+import prisma from "../../../prisma/prisma";
+import { Post } from "../../../types/posts";
+import { DerailleurResponse, createErrorResponse, createSuccessfulResponse } from "../../utils/responseGenerators";
+
+export interface UpdatePostPayload {
+  title: string;
+  content: string;
+  published?: boolean;
+}
+
+export type UpdatePost = (
+  updatePostPayload: UpdatePostPayload,
+  userId: string,
+) => Promise<DerailleurResponse<Post>>;
+
+export async function updatePost(updatePostPayload: UpdatePostPayload, postId: string, authorId: string): Promise<DerailleurResponse<Post>> {
+  const { content, title, published } = updatePostPayload;
+  try {
+    const updatedPost = await prisma.posts.update({
+      where: {
+        id: postId,
+        authorId: authorId
+      },
+      data: {
+        content,
+        title,
+        published,
+      }
+    });
+    return createSuccessfulResponse(updatedPost);
+  } catch (error: any) {
+    return createErrorResponse(error);
+  }
+};
