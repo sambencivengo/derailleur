85423a8fe5c58e07549087cce8b850f75a766ae5
Sam Bencivengo <sambencivengo@gmail.com>
2023-10-08 12:12:38 -0600

    updates to package.json, removing chakra and looking to combine tailwind with another library. Removing the american airlines env dep as I am not using it for now

--- src/app/components/CreateUser.tsx
+++ src/app/components/CreateUser.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { createUser } from '../queries/users/createUser';
-import { Button, HStack } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 
 export async function CreateUser() {
   return (
