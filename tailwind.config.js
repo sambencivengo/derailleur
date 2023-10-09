c6c638fb05984aff6b2cb128e0d9cffa5b4d2a49
Sam Bencivengo <sambencivengo@gmail.com>
2023-10-09 08:57:07 -0600

    tailwind adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    // './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -16,5 +16,5 @@ const config: Config = {
     },
   },
   plugins: [],
-};
-export default config;
+}
+
