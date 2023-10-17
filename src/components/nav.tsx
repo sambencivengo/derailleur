687bdaa145d14fd4b78b648e9a6294a7409ebcf0
Sam Bencivengo <sambencivengo@gmail.com>
2023-10-17 09:47:39 -0600

    toggle theme button fixed

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,6 +1,8 @@
 'use client';
 import Image from 'next/image';
 import { ToggleDarkModeButton } from './ui/toggleDarkModeButton';
+import { Sun, Moon } from 'lucide-react';
+import { Button } from './ui/button';
 
 export default function Nav() {
   return (
