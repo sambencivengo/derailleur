7045805a390fb339dbd6abd32e79c542ccb4fb67
Sam Bencivengo <sambencivengo@gmail.com>
2023-11-27 15:36:09 -0700

    very rudimentary auth, log in and sign up implemented

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,19 @@
+import { Form } from '~/components';
+import { Button, Container } from '~/components/ui';
+
 export default function Page() {
-  return <h1>Log in or sign up page</h1>;
+  return (
+    <Container>
+      <h1>Log in</h1>
+      <Form action="/api/login">
+        <label htmlFor="username">Username</label>
+        <input name="username" id="username" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <br />
+        <Button type="submit">Log In</Button>
+      </Form>
+    </Container>
+  );
 }
