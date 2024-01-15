93064a155797b54fbbf335a06c5bcd2399eddb63
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-15 10:51:56 -0400

    get Page session import type updated

--- src/auth/getPageSession.ts
+++ src/auth/getPageSession.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import * as context from 'next/headers';
 import { auth } from "~/auth/lucia";
-import { UserSession } from "~/types/models/sessions";
+import { UserSession } from "~/types";
 
 export const getPageSession = cache(() => {
   const authRequest = auth.handleRequest("GET", context);
