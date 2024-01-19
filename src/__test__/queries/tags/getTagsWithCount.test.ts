a1bf0b1454e28f4ee16ed33c2f05f500db908deb
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-19 10:23:53 -0400

    get tags with count test passing

---
+++ src/__test__/queries/tags/getTagsWithCount.test.ts
@@ -0,0 +1,103 @@
+import assert from 'assert';
+import { v4 as uuid } from 'uuid';
+import { mockUser_00 } from '~/__test__/mock/users/mockUser';
+import { addRecordsToDb, checkErrorResponse, cleanUpTable } from "~/__test__/utils";
+import { createPost, createUser, getTagsWithCount } from "~/queries";
+import { CreatePost, CreatePostPayload, CreateUser, PostWithTags, User } from "~/types";
+import prisma from '~prisma/prisma';
+
+
+const testUser00 = mockUser_00;
+const testUserId00 = uuid();
+const postId00 = uuid();
+const postId01 = uuid();
+
+const testTag00 = { name: "BIKEPACKING", expectedCount: 3 };
+const testTag01 = { name: "RIG", expectedCount: 3 };
+const testTag02 = { name: "VINTAGE", expectedCount: 3 };
+const testTag03 = { name: "TRIP REPORT", expectedCount: 4 };
+const testTag04 = { name: "TREK", expectedCount: 1 };
+const testTag05 = { name: "COLORADO", expectedCount: 1 };
+const arrayOfTestTags00 = [
+  testTag00,
+  testTag01,
+  testTag02,
+  testTag03,
+  testTag04,
+  testTag05,
+];
+
+describe("Get Tags with Count", function () {
+  const testTagNames00 = [
+    testTag00.name,
+    testTag01.name,
+    testTag02.name,
+    testTag03.name,
+  ];
+  const testTagsNames01 = [
+    testTag03.name,
+    testTag04.name,
+    testTag05.name,
+  ];
+  const testPostPayload00: CreatePostPayload = {
+    content: "Test content 00",
+    title: "Test Title 00",
+    tags: testTagNames00,
+  };
+  const testPostPayload01: CreatePostPayload = {
+    content: "Test content 01",
+    title: "Test Title 01",
+    tags: testTagNames00,
+  };
+  const testPostPayload02: CreatePostPayload = {
+    content: "Test content 02",
+    title: "Test Title 02",
+    tags: testTagNames00,
+  };
+  const testPostPayload03: CreatePostPayload = {
+    content: "Test content 03",
+    title: "Test Title 03",
+    tags: testTagsNames01,
+  };
+
+  beforeAll(async function () {
+    await addRecordsToDb<User, CreateUser>(
+      {
+        createRecordFunction: createUser,
+        newRecordParams: [
+          [{ username: testUser00.username }, testUserId00],
+        ],
+        mockDataName: 'User'
+      },
+    );
+    await addRecordsToDb<PostWithTags, CreatePost>(
+      {
+        createRecordFunction: createPost,
+        newRecordParams: [
+          [testPostPayload00, testUserId00, postId00],
+          [testPostPayload01, testUserId00],
+          [testPostPayload02, testUserId00],
+          [testPostPayload03, testUserId00, postId01],
+        ],
+        mockDataName: 'Post'
+      },
+    );
+  });
+
+  it("Successfully gets tags from the database with each tag's post count", async function () {
+    const response = await getTagsWithCount();
+    assert.ok(response.result);
+    checkErrorResponse(response.errors);
+    const tags = response.result;
+    assert.strictEqual(tags.length, arrayOfTestTags00.length, 'Expected tags length does not match result');
+    for (let i = 0, limi = tags.length; i < limi; i++) {
+      const resultTag = tags[i];
+      const foundTestTag = arrayOfTestTags00.find((tag) => tag.name === resultTag.name)!;
+      assert.strictEqual(resultTag._count.posts, foundTestTag.expectedCount, 'Result tag posts count does not match expected count');
+    }
+  });
+
+  afterAll(async function () {
+    await cleanUpTable([prisma.user, prisma.post, prisma.tag]);
+  });
+});
---
+++ src/queries/tags/getTagsWithCount.ts
@@ -0,0 +1,23 @@
+'use server';
+import { Prisma } from '@prisma/client';
+import prisma from '~prisma/prisma';
+import { createSuccessfulResponse, createErrorResponse, DerailleurResponse } from '~/utils';
+import { TagWithPostCount } from '~/types';
+
+
+export async function getTagsWithCount(): Promise<DerailleurResponse<TagWithPostCount[]>> {
+  try {
+    const tagWithPosts = await prisma.tag.findMany({
+      include: {
+        _count: true,
+      }
+    });
+    return createSuccessfulResponse(tagWithPosts);
+  } catch (error: any) {
+    if (!(error instanceof Prisma.PrismaClientKnownRequestError)) {
+      return createErrorResponse([{ message: 'An error occurred when retrieving all tags', data: { error: JSON.stringify(error) } }]);
+    }
+    const errResponse = { prismaErrorCode: error.code };
+    return createErrorResponse([{ message: 'Unable to get tags due to a prisma error', data: errResponse }]);
+  }
+}
