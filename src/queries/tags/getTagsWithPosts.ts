a17c2ee13eb4633f7948fb7f9aa4a9817251be44
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-19 10:37:09 -0400

    getTagsWithPosts query file

---
+++ src/queries/tags/getTagsWithPosts.ts
@@ -0,0 +1,28 @@
+'use server';
+import { Prisma } from '@prisma/client';
+import prisma from '~prisma/prisma';
+import { createSuccessfulResponse, createErrorResponse, DerailleurResponse } from '~/utils';
+import { TagWithPosts } from '~/types';
+
+
+export async function getTagsWithPosts(): Promise<DerailleurResponse<TagWithPosts[]>> {
+  try {
+    const tagsWithCount = await prisma.tag.findMany({
+      include: {
+        _count: true,
+        posts: {
+          include: {
+            tags: true
+          }
+        }
+      },
+    });
+    return createSuccessfulResponse(tagsWithCount);
+  } catch (error: any) {
+    if (!(error instanceof Prisma.PrismaClientKnownRequestError)) {
+      return createErrorResponse([{ message: 'An error occurred when retrieving all tags with posts', data: { error: JSON.stringify(error) } }]);
+    }
+    const errResponse = { prismaErrorCode: error.code };
+    return createErrorResponse([{ message: 'Unable to get tags due to a prisma error', data: errResponse }]);
+  }
+}
