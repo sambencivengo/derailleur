eb9b98a9006c6f870dbe7324af0dfd1a4903541f
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-21 11:32:51 -0700

    light mode the default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     // NOTE: suppressHydrationWarning comes from shadcn dark mode/theme implementation
     <html lang="en" suppressHydrationWarning>
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <Nav />
           <div className="my-5 px-5 lg:px-56 md:px-24 sm:px-14">{children}</div>
         </ThemeProvider>
