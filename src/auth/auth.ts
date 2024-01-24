b19fd3f74c0ed3449245bbc777cf27392f607134
Sam Bencivengo <sambencivengo@gmail.com>
2024-01-24 09:36:07 -0700

    oslo experimental package set in next config, auth file updated to bare prisma/next setup

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,24 +1,51 @@
 
 // auth/lucia.ts
-import { lucia } from "lucia";
-import { prisma } from "@lucia-auth/adapter-prisma";
-import { nextjs_future } from "lucia/middleware";
-import prismaClient from '../../prisma/prisma';
+// import { lucia } from "lucia";
+// import { prisma } from "@lucia-auth/adapter-prisma";
+// import { nextjs_future } from "lucia/middleware";
 
 
-export const auth = lucia({
-  adapter: prisma(prismaClient),
-  env: process.env.NODE_ENV === "development" ? "DEV" : "PROD",
-  middleware: nextjs_future(),
-  sessionCookie: {
-    expires: false
-  },
 
-  getUserAttributes: (data) => {
-    return {
-      username: data.username
-    };
+// auth/lucia.ts
+import { Lucia } from "lucia";
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+
+import prismaClient from '~prisma/prisma';
+
+const adapter = new PrismaAdapter(prismaClient.session, prismaClient.user);
+
+
+export const auth = new Lucia(
+  adapter,
+  {
+    sessionCookie: {
+      // this sets cookies with super long expiration
+      // since Next.js doesn't allow Lucia to extend cookie expiration when rendering pages
+      // https://v3.lucia-auth.com/getting-started/nextjs-app
+      expires: false,
+      attributes: {
+        secure: process.env.NODE_ENV === "production",
+      }
+    }
+    // {
+    // adapter: prisma(prismaClient),
+    // env: process.env.NODE_ENV === "development" ? "DEV" : "PROD",
+    // middleware: nextjs_future(),
+    // sessionCookie: {
+    //   expires: false
+    // },
+
+    // getUserAttributes: (data) => {
+    //   return {
+    //     username: data.username
+    //   };
+    // }
   }
-});
+);
 
-export type Auth = typeof auth;
+// IMPORTANT!
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof auth;
+  }
+}
