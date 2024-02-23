5bcfe50568228a22667d7191adecf3e7a97158da
Sam Bencivengo <sambencivengo@gmail.com>
2024-02-22 17:14:01 -0700

    comment count added to post preview

--- src/components/postPreview.tsx
+++ src/components/postPreview.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import moment from 'moment';
 import { Card, CardHeader, CardTitle, CardDescription, CardFooter, Badge } from '~/components/ui';
 import { Post } from '~/types';
+import { MessageSquare } from 'lucide-react';
 
 interface PostPreviewProps {
   post: Post;
@@ -26,24 +27,30 @@ export function PostPreview({ post }: PostPreviewProps) {
 
   return (
     <Card className="h-auto">
-      <CardHeader>
-        <CardTitle className="text-base truncate">
+      <CardHeader className="h-auto gap-y-2">
+        <CardTitle className="text-base line-clamp-2">
           <Link className="hover:text-primary" href={`/post/${id}`}>
             {post.title}
           </Link>
         </CardTitle>
-        <div className="flex flex-row space-x-1">
-          <CardDescription>
-            by{' '}
-            <Link className="underline hover:text-primary" href={`/user/${authorId}`}>
-              {username}
-            </Link>
-          </CardDescription>
-          <CardDescription> {moment(createdAt).fromNow()}</CardDescription>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-row gap-x-1">
+            <CardDescription>
+              by{' '}
+              <Link className="underline hover:text-primary" href={`/user/${authorId}`}>
+                {username}
+              </Link>
+            </CardDescription>
+            <CardDescription> {moment(createdAt).fromNow()}</CardDescription>
+          </div>
+          <Link className="relative flex flex-col justify-center items-center hover:text-primary" href={`/post/${id}`}>
+            <MessageSquare />
+            <p className="absolute top-[80%] text-sm">{post._count.comments}</p>
+          </Link>
         </div>
       </CardHeader>
       <CardFooter>
-        <div className="flex flex-grow gap-3">{renderTagBadges}</div>
+        <div className="flex flex-wrap gap-3">{renderTagBadges}</div>
       </CardFooter>
     </Card>
   );
