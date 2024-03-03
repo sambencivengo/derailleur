a1828227fb132891f63cad279d919587c0405add
Sam Bencivengo <sambencivengo@gmail.com>
2024-03-03 10:42:28 -0700

    moved post query to server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { PostPreviewContainer } from '~/components';
-import { getPosts } from '~/queries';
 
 export default async function Home() {
-  const posts = await getPosts();
   return (
     <main>
-      <PostPreviewContainer postsResponse={posts} />
+      <PostPreviewContainer />
     </main>
   );
 }
--- src/components/postPreviewContainer.tsx
+++ src/components/postPreviewContainer.tsx
@@ -4,20 +4,17 @@ import { AlertCircle } from 'lucide-react';
 import React from 'react';
 import { PostPreview } from '~/components';
 import { Alert, AlertTitle, AlertDescription } from '~/components/ui';
-import { Post } from '~/types';
-import { DerailleurResponse } from '~/utils';
+import { getPosts } from '~/queries';
 
-interface PostPreviewContainerProps {
-  postsResponse: DerailleurResponse<Post[]>;
-}
+export async function PostPreviewContainer() {
+  const posts = await getPosts();
 
-export async function PostPreviewContainer({ postsResponse }: PostPreviewContainerProps) {
-  if (postsResponse.result === null || postsResponse.errors.length > 0) {
+  if (posts.result === null || posts.errors.length > 0) {
     return (
       <Alert variant="destructive">
         <AlertCircle className="h-4 w-4" />
         <AlertTitle>Error</AlertTitle>
-        {postsResponse.errors.map(({ message }, idx) => {
+        {posts.errors.map(({ message }, idx) => {
           return <AlertDescription key={idx}>{message}</AlertDescription>;
         })}
       </Alert>
@@ -26,7 +23,7 @@ export async function PostPreviewContainer({ postsResponse }: PostPreviewContain
 
   return (
     <div className="space-y-2">
-      {postsResponse.result!.map((post, idx) => {
+      {posts.result!.map((post, idx) => {
         return <PostPreview post={post} key={idx} />;
       })}
     </div>
