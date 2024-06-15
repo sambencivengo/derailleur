e5aff33d653c556dcaa6d86d0e1041ea8983a586
Sam Bencivengo <sambencivengo@gmail.com>
2024-06-15 14:50:28 -0400

    config and package updates

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['oslo'],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
