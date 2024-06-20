b8e226b913e47d83b780a70b2924e82b9a21c784
Sam Bencivengo <sambencivengo@gmail.com>
2024-06-20 13:24:13 -0400

    comment query type and payload

--- src/types/queries/comments.queries.types.ts
+++ src/types/queries/comments.queries.types.ts
@@ -2,14 +2,10 @@ import { Comment } from "~/types";
 import { DerailleurResponse } from "~/utils";
 
 export type CreateComment = (
-  createCommentPayload: CreateCommentPayload,
-  userId: string,
-  commentId?: string
+  createCommentPayload: CreateCommentPayload, postId: string, userId: string, parentId?: string, commentId?: string
 ) => Promise<DerailleurResponse<Comment>>;
 
 export interface CreateCommentPayload {
-  postId: string;
-  parentId?: string;
   content: string;
 }
 
