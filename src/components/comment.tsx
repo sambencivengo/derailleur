a5e9f329eb3621a611c04c88ad7ebda5928c30d8
Sam Bencivengo <sambencivengo@gmail.com>
2024-06-20 13:20:22 -0400

    comment component with some temp type stuff

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,27 +1,54 @@
 import moment from 'moment';
+import Link from 'next/link';
+import React from 'react';
+import { CommentReplyForm } from '~/components/commentReplyForm';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader } from '~/components/ui';
 
 interface CommentProps {
-  comment: any;
+  comment: CommentWithRepliesOrCount;
+  userId: string | null;
+  level: number;
+}
+interface CommentWithRepliesAndAuthor {
+  id: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  authorId: string;
+  postId: string;
+  parentCommentId: string;
+  author: { username: string; id: string };
+  replies: CommentWithRepliesAndAuthor[];
 }
 
-export async function Comment({ comment }: CommentProps) {
-  const { content, createdAt, replies, author } = comment;
+interface CommentWithRepliesOrCount extends CommentWithRepliesAndAuthor {
+  _count: { replies: number };
+}
 
+export async function Comment({ comment, userId, level }: CommentProps) {
+  const { content, createdAt, replies, author, postId, id } = comment as CommentWithRepliesOrCount;
   return (
     <Card className="w-full">
       <CardHeader>
         <p className="font-bold">{author.username}</p>
       </CardHeader>
-      <CardContent>
+      <CardContent className="w-full">
         <p>{content}</p>
-        <CardDescription>{moment(createdAt).format('LLL')}</CardDescription>
+        <div className="flex flex-row gap-x-2">
+          <CardDescription>{moment(createdAt).format('LLL')}</CardDescription>
+        </div>
+        <CommentReplyForm parentCommentId={id} postId={postId} userId={userId} />
       </CardContent>
-      <CardFooter className="flex flex-col gap-y-2">
-        {replies &&
+      <CardFooter className="flex flex-col p-0 pl-5 pr-1 pb-1 gap-y-2">
+        {level === 4 ? (
+          <Link href={`/post/${postId}/comment/${comment.id}`} className="text-primary hover:underline">
+            ...load more comments
+          </Link>
+        ) : (
           replies.map((comment: any, idx: number) => {
-            return <Comment key={idx} comment={comment} />;
-          })}
+            return <Comment key={idx} comment={comment} userId={userId} level={level + 1} />;
+          })
+        )}
       </CardFooter>
     </Card>
   );
