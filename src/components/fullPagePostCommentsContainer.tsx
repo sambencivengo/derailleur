a84931aac4ffa1ba45629475f68cfcad7be018a6
Sam Bencivengo <sambencivengo@gmail.com>
2024-06-20 13:16:10 -0400

    fiull page post comments container

--- src/components/fullPagePostCommentsContainer.tsx
+++ src/components/fullPagePostCommentsContainer.tsx
@@ -2,19 +2,17 @@
 import { getUserSession } from '~/auth';
 import { Comment, QueryError } from '~/components';
 import { getComments } from '~/queries/comments/getComments';
+import { CommentWithAuthorUsernameIDAndReplies } from '~/types';
 
 interface FullPagePostCommentsContainerProps {
   postId: string;
-  userId: string | null;
+  userId?: string;
+  commentId?: string;
 }
 
-export async function FullPagePostCommentsContainer({ postId }: FullPagePostCommentsContainerProps) {
-  // TODO: find elegant way to query x number of parent comments
-  // Parent comments belong to this postId and have no parent themselves
-
-  // TODO: dummy get
+export async function FullPagePostCommentsContainer({ postId, commentId }: FullPagePostCommentsContainerProps) {
   const user = await getUserSession();
-  const response = await getComments(postId);
+  const response = await getComments(postId, commentId);
   const { errors, result } = response;
   if (errors.length > 0 || result === null) {
     return <QueryError errors={errors} />;
@@ -22,8 +20,9 @@ export async function FullPagePostCommentsContainer({ postId }: FullPagePostComm
   return (
     <>
       <div className="flex flex-col">
-        {result.map((comment, idx) => {
-          return <Comment key={idx} comment={comment} userId={user ? user.userId : null} />;
+        {/* TODO: bespoke comment queries, get rid of this any */}
+        {result.map((comment: any, idx: number) => {
+          return <Comment key={idx} comment={comment} userId={user ? user.userId : null} level={0} />;
         })}
       </div>
     </>
