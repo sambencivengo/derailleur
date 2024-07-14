dc94373c6f13b8b9fa4b97eecd1c8bd858d05cb2
Sam Bencivengo <sambencivengo@gmail.com>
2024-07-13 22:41:19 -0600

    CommentWithAuthorUsernameIDAndReplies updates to be used correctly

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -3,30 +3,25 @@ import Link from 'next/link';
 import React from 'react';
 import { CommentReplyForm } from '~/components/commentReplyForm';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader } from '~/components/ui';
+import { CommentWithAuthorUsernameIDAndReplies } from '~/types';
 
 interface CommentProps {
-  comment: CommentWithRepliesOrCount;
+  comment: CommentWithAuthorUsernameIDAndReplies;
   userId: string | null;
   level: number;
 }
-interface CommentWithRepliesAndAuthor {
-  id: string;
-  content: string;
-  createdAt: Date;
-  updatedAt: Date;
-  authorId: string;
-  postId: string;
-  parentCommentId: string;
-  author: { username: string; id: string };
-  replies: CommentWithRepliesAndAuthor[];
-}
-
-interface CommentWithRepliesOrCount extends CommentWithRepliesAndAuthor {
-  _count: { replies: number };
-}
 
 export async function Comment({ comment, userId, level }: CommentProps) {
-  const { content, createdAt, replies, author, postId, id } = comment as CommentWithRepliesOrCount;
+  const {
+    content,
+    createdAt,
+    replies,
+    author,
+    postId,
+    id,
+    _count: { replies: repliesCount },
+  } = comment;
+  console.log({ repliesCount, level });
   return (
     <Card className="w-full">
       <CardHeader>
@@ -40,15 +35,16 @@ export async function Comment({ comment, userId, level }: CommentProps) {
         <CommentReplyForm parentCommentId={id} postId={postId} userId={userId} />
       </CardContent>
       <CardFooter className="flex flex-col p-0 pl-5 pr-1 pb-1 gap-y-2">
-        {level === 4 ? (
+        {level >= 4 && repliesCount > 0 && (
           <Link href={`/post/${postId}/comment/${comment.id}`} className="text-primary hover:underline">
             ...load more comments
           </Link>
-        ) : (
+        )}
+        {level < 4 &&
+          replies.length > 0 &&
           replies.map((comment: any, idx: number) => {
             return <Comment key={idx} comment={comment} userId={userId} level={level + 1} />;
-          })
-        )}
+          })}
       </CardFooter>
     </Card>
   );
