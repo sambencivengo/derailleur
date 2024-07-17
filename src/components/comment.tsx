807c51fbdc834a038cb9ef108760096e4f40116c
Sam Bencivengo <sambencivengo@gmail.com>
2024-07-17 11:00:36 -0600

    consolidated the replies, should probably update the response object so I don't have to pass the props individually int he future

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -8,23 +8,22 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader } from '~/co
 import { CommentWithAuthorUsernameIDAndReplies, SubmittedCommentWithAuthorUsernameAndId, UserAndSession } from '~/types';
 
 interface CommentProps {
-  comment: CommentWithAuthorUsernameIDAndReplies;
   user: UserAndSession | null;
   level: number;
+  content: string;
+  createdAt: Date;
+  replies: Array<CommentWithAuthorUsernameIDAndReplies>;
+  author: {
+    id: string;
+    username: string;
+  };
+  postId: string;
+  repliesCount: number;
+  commentId: string;
 }
 
-export function Comment({ comment, user, level }: CommentProps) {
+export function Comment({ author, commentId, content, createdAt, postId, replies, repliesCount, user, level }: CommentProps) {
   const [newCommentsOnComment, setNewCommentsOnComment] = React.useState<Array<SubmittedCommentWithAuthorUsernameAndId>>([]);
-
-  const {
-    content,
-    createdAt,
-    replies,
-    author,
-    postId,
-    _count: { replies: repliesCount },
-    id,
-  } = comment;
 
   return (
     <Card className="w-full">
@@ -38,50 +37,18 @@ export function Comment({ comment, user, level }: CommentProps) {
         </div>
       </CardContent>
       <CardFooter className="flex flex-col p-0 pl-5 pr-1 pb-1 gap-y-2">
-        <CommentLinks parentCommentId={id} postId={postId} user={user} setNewComments={setNewCommentsOnComment} isUsersComment={user !== null && user.userId === comment.authorId} />
+        <CommentLinks parentCommentId={commentId} postId={postId} user={user} setNewComments={setNewCommentsOnComment} isUsersComment={user !== null && user.userId === author.id} />
         {level >= 4 && repliesCount > 0 && (
-          <Link href={`/post/${postId}/comment/${id}`} className="text-primary hover:underline">
+          <Link href={`/post/${postId}/comment/${commentId}`} className="text-primary hover:underline">
             ...load more comments
           </Link>
         )}
-        {newCommentsOnComment.length > 0 && newCommentsOnComment.map(({ author, content, createdAt, id, postId, authorId }, idx) => <SubmittedCommentReply key={idx} authorId={authorId} username={author.username} postId={postId} user={user} content={content} createdAt={createdAt} commentId={id} />)}
+        {newCommentsOnComment.length > 0 && newCommentsOnComment.map(({ author, content, createdAt, id, postId }, idx) => <Comment key={idx} author={author} commentId={id} content={content} createdAt={createdAt} postId={postId} replies={[]} repliesCount={0} user={user} level={0} />)}
         {level < 4 &&
           replies.length > 0 &&
-          replies.map((comment: any, idx: number) => {
-            return <Comment key={idx} comment={comment} user={user} level={level + 1} />;
+          replies.map(({ _count: { replies: repliesCount }, author, content, createdAt, id, postId, replies }, idx: number) => {
+            return <Comment key={idx} author={author} commentId={id} content={content} postId={postId} createdAt={createdAt} user={user} replies={replies} repliesCount={repliesCount} level={level + 1} />;
           })}
-      </CardFooter>
-    </Card>
-  );
-}
-
-export interface SubmittedCommentReplyProps {
-  username: string;
-  user: UserAndSession | null;
-  commentId: string;
-  authorId: string;
-  content: string;
-  createdAt: Date;
-  postId: string;
-}
-
-export function SubmittedCommentReply({ user, username, content, createdAt, postId, commentId, authorId }: SubmittedCommentReplyProps) {
-  const [newCommentsOnComment, setNewCommentsOnComment] = React.useState<Array<SubmittedCommentWithAuthorUsernameAndId>>([]);
-
-  return (
-    <Card className="w-full">
-      <CardHeader>
-        <p className="font-bold">{username}</p>
-      </CardHeader>
-      <CardContent className="w-full">
-        <p>{content}</p>
-        <div className="flex flex-row gap-x-2">
-          <CardDescription>{moment(createdAt).format('LLL')}</CardDescription>
-        </div>
-      </CardContent>
-      <CardFooter className="flex flex-col p-0 pl-5 pr-1 pb-1 gap-y-2">
-        <CommentLinks parentCommentId={commentId} postId={postId} user={user} setNewComments={setNewCommentsOnComment} isUsersComment={user !== null && user.userId === authorId} />
-        {newCommentsOnComment.length > 0 && newCommentsOnComment.map(({ author, content, createdAt, id, postId, authorId }, idx) => <SubmittedCommentReply key={idx} authorId={authorId} username={author.username} postId={postId} user={user} content={content} createdAt={createdAt} commentId={id} />)}
       </CardFooter>
     </Card>
   );
--- src/components/commentsView.tsx
+++ src/components/commentsView.tsx
@@ -1,4 +1,4 @@
-import { Comment, SubmittedCommentReply } from '~/components/comment';
+import { Comment } from '~/components/comment';
 import { CommentWithAuthorUsernameIDAndReplies, SubmittedCommentWithAuthorUsernameAndId, UserAndSession } from '~/types';
 
 interface CommentsViewProps {
@@ -12,10 +12,10 @@ export function CommentsView({ user, comments, newCommentsOnPost }: CommentsView
     <div className="flex flex-col">
       {newCommentsOnPost.length > 0 &&
         newCommentsOnPost.map(({ author, content, createdAt, id, postId }, idx) => {
-          return <SubmittedCommentReply commentId={id} authorId={author.id} key={idx} content={content} createdAt={createdAt} postId={postId} user={user} username={author.username} />;
+          return <Comment key={idx} author={author} commentId={id} content={content} createdAt={createdAt} postId={postId} replies={[]} repliesCount={0} user={user} level={0} />;
         })}
-      {comments.map((comment, idx) => {
-        return <Comment key={idx} comment={comment} user={user} level={0} />;
+      {comments.map(({ _count, author, content, createdAt, id, postId, replies }, idx) => {
+        return <Comment key={idx} author={author} commentId={id} content={content} createdAt={createdAt} postId={postId} replies={replies} repliesCount={_count.replies} user={user} level={0} />;
       })}
     </div>
   );
