47ed53bfded1cc45a821a32451cff6a76ea69ca7
Sam Bencivengo <sambencivengo@gmail.com>
2024-07-17 21:09:10 -0600

    username links use user instead of id as slug

--- src/components/postPreview.tsx
+++ src/components/postPreview.tsx
@@ -10,7 +10,6 @@ interface PostPreviewProps {
 export function PostPreview({ post }: PostPreviewProps) {
   const {
     author: { username },
-    authorId,
     createdAt,
     id,
     tags,
@@ -41,7 +40,7 @@ export function PostPreview({ post }: PostPreviewProps) {
           <div className="flex flex-row gap-1">
             <CardDescription>
               by{' '}
-              <Link className="underline hover:text-primary" href={`/user/${authorId}`}>
+              <Link className="underline hover:text-primary" href={`/user/${username}`}>
                 {username}
               </Link>
             </CardDescription>
