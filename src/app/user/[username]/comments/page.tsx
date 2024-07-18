8507ac925ec75215674b4ca1336fd81e710e9a15
Sam Bencivengo <sambencivengo@gmail.com>
2024-07-17 21:06:01 -0600

    pages for comments and posts on profile, needs to be dryer in the future

--- src/app/user/[username]/comments/page.tsx
+++ src/app/user/[username]/comments/page.tsx
@@ -1,3 +1,34 @@
+import { getUserSession } from '~/auth';
+import { QueryError, TextHeading } from '~/components';
+import { Comment } from '~/components/comment';
+import { Separator } from '~/components/ui';
+import { getCommentsForProfile } from '~/queries/comments/getComments';
+
 export default async function Page({ params }: { params: { username: string } }) {
-  return <div className="flex flex-col justify-center gap-y-2"></div>;
+  const { username } = params;
+  const user = await getUserSession();
+  // TODO: DRY up code between this page and the profile posts page
+  const emptyCommentsString = user !== null && user.username === username ? "Looks like you haven't made any comments... Any comments you create will be shown here." : `${username} hasn't created any comments yet`;
+  const { errors, result } = await getCommentsForProfile(username);
+  if (result === null || errors.length > 0) {
+    return <QueryError errors={errors} />;
+  } else if (result.length === 0) {
+    return (
+      <div className="mt-10">
+        <TextHeading heading={emptyCommentsString} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col mt-5 gap-2">
+        <TextHeading heading={user !== null && user.username === username ? 'Your comments' : `Comments by ${username}`} />
+        <Separator />
+        <div className="space-y-2 flex flex-col">
+          {result.map(({ author, content, createdAt, id, postId, updatedAt }, idx) => {
+            return <Comment key={idx} author={author} commentId={id} content={content} createdAt={createdAt} postId={postId} replies={[]} updatedAt={updatedAt} repliesCount={0} user={user} level={0} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
--- src/app/user/[username]/posts/page.tsx
+++ src/app/user/[username]/posts/page.tsx
@@ -1,3 +1,33 @@
+import { getUserSession } from '~/auth';
+import { PostPreview, QueryError, TextHeading } from '~/components';
+import { Separator } from '~/components/ui';
+import { getPosts } from '~/queries';
+
 export default async function Page({ params }: { params: { username: string } }) {
-  return <div className="flex flex-col justify-center gap-y-2"></div>;
+  const { username } = params;
+  const user = await getUserSession();
+  // TODO: DRY up code between this page and the profile comments page
+  const emptyPostsString = user !== null && user.username === username ? "Looks like you haven't made any posts... Any posts you create will be shown here." : `${username} hasn't created any posts yet`;
+  const { errors, result } = await getPosts(username);
+  if (result === null || errors.length > 0) {
+    return <QueryError errors={errors} />;
+  } else if (result.length === 0) {
+    return (
+      <div className="mt-10">
+        <TextHeading heading={emptyPostsString} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col mt-5 gap-2">
+        <TextHeading heading={user !== null && user.username === username ? 'Your posts' : `Posts by ${username}`} />
+        <Separator />
+        <div className="space-y-2">
+          {result.map((post, idx) => {
+            return <PostPreview post={post} key={idx} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
