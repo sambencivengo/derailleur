4b4184b6e02c073d150088d864626ff955fd6e7b
Sam Bencivengo <sambencivengo@gmail.com>
2024-09-04 15:55:40 -0600

    update to images and post previews

--- src/components/postPreview.tsx
+++ src/components/postPreview.tsx
@@ -2,9 +2,9 @@
 import React from 'react';
 import Link from 'next/link';
 import moment from 'moment';
-import { Card, CardHeader, CardTitle, CardDescription, CardFooter, Badge, CardContent, Button } from '~/components/ui';
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter, Badge, Button } from '~/components/ui';
 import { PostWithAuthorNameTagsAndCommentCount, UserAndSession } from '~/types';
-import { Heart, MessageSquare } from 'lucide-react';
+import { Bookmark, Heart, MessageSquare } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
 import { PostCategoryTag } from '~/components/postCategoryTag';
 import { useLikePost } from '~/hooks/useLikePost';
@@ -26,6 +26,7 @@ export function PostPreview({ post, user }: PostPreviewProps) {
     id,
     tags,
   } = post;
+
   const renderTagBadges = tags.map((tag, idx) => {
     return (
       <Link key={idx} href={`/tags/${tag.name.toLowerCase().split(' ').join('-')}`}>
@@ -34,9 +35,9 @@ export function PostPreview({ post, user }: PostPreviewProps) {
     );
   });
   return (
-    <Card className="h-auto flex flex-row justify-between w-full hover:border-primary hover:border-1">
-      <div className="flex flex-col h-full">
-        <CardHeader className="h-auto gap-y-2 p-3">
+    <Card className="w-full flex flex-col">
+      <div className="w-full flex justify-between flex-row">
+        <CardHeader className="h-auto p-3">
           <div className="flex flex-col gap-2">
             <CardTitle className="text-base line-clamp-2">
               <Link className="hover:text-primary" href={`/post/${id}`}>
@@ -49,64 +50,61 @@ export function PostPreview({ post, user }: PostPreviewProps) {
               {renderTagBadges}
             </div>
           </div>
-          <div className="flex flex-col">
-            <div className="flex flex-row gap-1">
-              <CardDescription>
-                by{' '}
-                <Link className="underline hover:text-primary" href={`/user/${username}/posts`}>
-                  {username}
-                </Link>
-              </CardDescription>
-              <CardDescription> {moment(createdAt).fromNow()}</CardDescription>
-            </div>
+          <div className="flex flex-row gap-1">
+            <CardDescription className="text-xs">
+              by{' '}
+              <Link className="underline hover:text-primary" href={`/user/${username}/posts`}>
+                {username}
+              </Link>{' '}
+              {moment(createdAt).fromNow()}
+            </CardDescription>
           </div>
-        </CardHeader>
-        <CardContent className="p-0 gap-4">
-          <CardFooter className="pb-2 pl-3 flex flex-wrap text-primary">
-            <div className="flex flex-row gap-2 items-center">
-              <Button
-                className="h-6 w-6 hover:bg-opacity-0"
-                size={'icon'}
-                variant={'link'}
-                onClick={() => {
-                  if (user === null) {
-                    router.push(`/login?returnPath=${pathName}`);
-                  } else {
-                    handleLikePost(user.userId);
-                  }
-                }}
-              >
-                {liked ? <Heart className={'text-primary'} fill={'#f97316'} /> : <Heart className={'text-primary'} />}
-              </Button>
-              <Link href={`/post/${id}`}>
-                <Button variant={'link'}>
-                  {numberOfLikes} like{numberOfLikes > 1 || numberOfLikes === 0 ? 's' : ''}
-                </Button>
-              </Link>
-            </div>
-            <Link className="text-[16px]" href={`/post/${id}`}>
-              <Button variant="link" className="flex-row gap-1 ">
-                {post._count.comments}
-                <MessageSquare className="top-[1px]" />
-              </Button>
-            </Link>
-
-            <Button
-              onClick={() => {
-                if (user === null) {
-                  router.push(`/login?returnPath=${pathName}`);
-                } else {
-                  handleSavePost(user.userId);
-                }
-              }}
-              variant="link"
-            >
-              {saved ? 'Unsave' : 'Save'}
-            </Button>
-          </CardFooter>
-        </CardContent>
+        </CardHeader>{' '}
+        <PostPreviewThumbnail postId={post.id} rideWithGPSLink={post.rideWithGPSLink} thumbnail={post.thumbnail} />
       </div>
-      <PostPreviewThumbnail postId={post.id} rideWithGPSLink={post.rideWithGPSLink} thumbnail={post.thumbnail} />
+      <CardFooter className="w-full pb-2 pl-3 flex flex-wrap text-primary">
+        <div className="flex flex-row gap-2 items-center">
+          <Button
+            className="h-6 w-6 hover:bg-opacity-0"
+            size={'icon'}
+            variant={'link'}
+            onClick={() => {
+              if (user === null) {
+                router.push(`/login?returnPath=${pathName}`);
+              } else {
+                handleLikePost(user.userId);
+              }
+            }}
+          >
+            {liked ? <Heart className={'text-primary'} fill={'#f97316'} /> : <Heart className={'text-primary'} />}
+          </Button>
+          <Link href={`/post/${id}`}>
+            <Button variant={'link'}>
+              {numberOfLikes} like{numberOfLikes > 1 || numberOfLikes === 0 ? 's' : ''}
+            </Button>
+          </Link>
+        </div>
+        <Link className="text-[16px]" href={`/post/${id}`}>
+          <Button variant="link" className="flex-row gap-1 ">
+            {post._count.comments}
+            <MessageSquare className="top-[1px]" />
+          </Button>
+        </Link>
+
+        <Button
+          size={'icon'}
+          variant={'link'}
+          onClick={() => {
+            if (user === null) {
+              router.push(`/login?returnPath=${pathName}`);
+            } else {
+              handleSavePost(user.userId);
+            }
+          }}
+        >
+          {saved ? <Bookmark className={'text-primary'} fill={'#f97316'} /> : <Bookmark className={'text-primary'} />}
+        </Button>
+      </CardFooter>
     </Card>
   );
 }
