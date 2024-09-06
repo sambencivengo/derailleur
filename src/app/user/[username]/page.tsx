a02f58bd32b514f78edc698319ecd0c55e74a271
Sam Bencivengo <sambencivengo@gmail.com>
2024-09-05 18:15:20 -0600

    comment out unused function

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -7,20 +7,20 @@ export default async function Page({ params }: { params: { username: string } })
 
   const { errors, result } = await getUserByUsername(username);
 
-  function getCityState() {
-    navigator.geolocation.getCurrentPosition(
-      (position: GeolocationPosition) => {
-        const { longitude, latitude } = position.coords;
+  // function getCityState() {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position: GeolocationPosition) => {
+  //       const { longitude, latitude } = position.coords;
 
-        console.log('Your current position is:');
-        console.log(`Latitude : ${latitude}`);
-        console.log(`Longitude: ${longitude}`);
-      },
-      (e: GeolocationPositionError) => {
-        return e;
-      }
-    );
-  }
+  //       console.log('Your current position is:');
+  //       console.log(`Latitude : ${latitude}`);
+  //       console.log(`Longitude: ${longitude}`);
+  //     },
+  //     (e: GeolocationPositionError) => {
+  //       return e;
+  //     }
+  //   );
+  // }
   if (result === null || errors.length > 0) {
     return <QueryError errors={errors} />;
   } else {
