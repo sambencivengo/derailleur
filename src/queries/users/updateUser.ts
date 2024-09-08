4609042a9e7d231fba458cb4f61fc74bf48cc7ae
Sam Bencivengo <sambencivengo@gmail.com>
2024-09-08 09:24:58 -0600

    validate schema is added

--- src/queries/users/updateUser.ts
+++ src/queries/users/updateUser.ts
@@ -1,24 +1,32 @@
 'use server';
 import { Prisma } from "@prisma/client";
+import { editProfileSchema, EditProfileSchema, validateSchema } from "~/schemas";
 import { UpdateUserPayload, UserProfile, userProfile } from "~/types";
-import { DerailleurResponse, createSuccessfulResponse, createErrorResponse } from "~/utils";
+import { DerailleurResponse, createSuccessfulResponse, createErrorResponse, DerailleurError } from "~/utils";
 import prisma from "~prisma/prisma";
 
-export async function updateUser(user: UpdateUserPayload, userId: string,): Promise<DerailleurResponse<UserProfile>> {
+export async function updateUser(updateUserPayload: UpdateUserPayload, userId: string,): Promise<DerailleurResponse<UserProfile>> {
   try {
-    // TODO: schema validation
+    const validateResponse = validateSchema<EditProfileSchema>({ body: updateUserPayload, schema: editProfileSchema });
+    if (validateResponse.result === null || validateResponse.errors.length > 0) {
+      const errors: DerailleurError[] = validateResponse.errors.map((error) => {
+        return { data: updateUserPayload, message: error.message };
+      });
+      return (createErrorResponse(errors));
+    }
+    const { favoriteBikes, location } = validateResponse.result;
     const updatedUser = await prisma.user.update({
       where: { id: userId },
       data: {
-        location: user.location === '' ? null : user.location,
-        favoriteBikes: user.favoriteBikes
+        location: location === '' ? null : updateUserPayload.location,
+        favoriteBikes
       },
       ...userProfile
     });
     return (createSuccessfulResponse(updatedUser));
   } catch (error: any) {
     if (!(error instanceof Prisma.PrismaClientKnownRequestError)) {
-      return createErrorResponse([{ message: 'An error occurred when attempting to update a user', data: { user, userId, error: JSON.stringify(error) } }]);
+      return createErrorResponse([{ message: 'An error occurred when attempting to update a user', data: { user: updateUserPayload, userId, error: JSON.stringify(error) } }]);
     }
     const errResponse = { userId, prismaErrorCode: error.code };
     return createErrorResponse([{ message: 'Unable to update user due to prisma error', data: errResponse }]);
