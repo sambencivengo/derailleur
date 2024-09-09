9eb5069b28d94ce9e23f4a16eba0135d8505596e
Sam Bencivengo <sambencivengo@gmail.com>
2024-09-09 10:59:25 -0600

    removed unused variable

--- src/components/signUpButton.tsx
+++ src/components/signUpButton.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
 import { Button } from '~/components/ui';
-import { cn } from '~/lib/utils';
 
 interface SignUpButtonProps {
   forMobile?: boolean;
