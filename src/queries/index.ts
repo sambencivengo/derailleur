53fba0169517e4e9e14854a917f800239b6fa1c8
Sam Bencivengo <sambencivengo@gmail.com>
2024-11-18 22:14:00 -0500

    first star export file

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,4 +1,4 @@
-export * from './posts';
-export * from './users';
-export * from "./tags";
-export * from './comments';
+export { createPost, getPostById, getPosts, getSavedPost, getSavedPosts, savePost, unsavePost, updatePost } from './posts';
+export { createUser, getUserById, getUserByUsername, updateUser } from './users';
+export { getTagWithCountById, getTagWithCountByName, getTagWithPostsById, getTagWithPostsByName, getTagsWithCount, getTagsWithCountByName, getTagsWithPostsByName } from "./tags";
+export { createComment } from './comments';
