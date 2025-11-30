a5454425231edadeb6212b9283d3c7b4cadb3df3
Sam Bencivengo <sambencivengo@gmail.com>
2025-11-30 12:37:37 -0800

    test updated

--- src/__test__/drizzle/users/users.test.ts
+++ src/__test__/drizzle/users/users.test.ts
@@ -6,14 +6,14 @@ import { eq } from 'drizzle-orm';
 
 describe('User Database Operations', () => {
   const username = "Sammy";
-  const password = "password123";
+  const hashedPassword = "password123";
   const favoriteBikes = ["1991 Trek 990", "Omnium CXC"];
   const location = "Brooklyn, NY";
 
   test('add user to database', async () => {
-    const insertedUser = await db.insert(users).values({ username, password, favoriteBikes, location }).returning();
+    const insertedUser = await db.insert(users).values({ username, hashedPassword, favoriteBikes, location }).returning();
     assert(insertedUser[0].username === username);
-    assert(insertedUser[0].password === password);
+    assert(insertedUser[0].hashedPassword === hashedPassword);
     assert(insertedUser[0].location === location);
     insertedUser[0].favoriteBikes?.forEach((returnedBike, idx) => {
       assert(returnedBike === favoriteBikes[idx])
@@ -21,7 +21,7 @@ describe('User Database Operations', () => {
   });
 
   test('retrieve user from database', async () => {
-    const [insertedUser] = await db.insert(users).values({ username, password, favoriteBikes, location }).returning();
+    const [insertedUser] = await db.insert(users).values({ username, hashedPassword, favoriteBikes, location }).returning();
     const returnedUser = await db.query.users.findFirst({ where: eq(users.id, insertedUser.id) });
     assert(returnedUser?.username === username);
     assert(returnedUser?.id === insertedUser.id);
